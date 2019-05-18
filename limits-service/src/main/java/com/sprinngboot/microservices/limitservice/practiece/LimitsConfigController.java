59a50f8ecc7aaa2289bcee4ca8082c2f41110b57
Gurubabu Jampala <j.gurubabu@gmail.com>
2019-05-19 00:27:37 +0530

    changes done for adding the zipkin and hystrix

--- limits-service/src/main/java/com/sprinngboot/microservices/limitservice/practiece/LimitsConfigController.java
+++ limits-service/src/main/java/com/sprinngboot/microservices/limitservice/practiece/LimitsConfigController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.ribbon.proxy.annotation.Hystrix;
 import com.sprinngboot.microservices.limitservice.practiece.bean.LimitsConfiguration;
 
 @RestController
@@ -16,5 +17,13 @@ public class LimitsConfigController {
 	public LimitsConfiguration fetchLimitsConfiguration(){
 		return new LimitsConfiguration(configuration.getMinimum(), configuration.getMaximum());
 	}
+	
+	@GetMapping("/fault-tolerance-limits")
+//	@Hystrix(fallbackHandler="fetchLimitConfigurationForFault")
+	public LimitsConfiguration fetchLimitsConfiguration1(){
+		return new LimitsConfiguration(configuration.getMinimum(), configuration.getMaximum());
+	}
+	
+	
 	
 }
